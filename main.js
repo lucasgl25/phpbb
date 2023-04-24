5348447f2dbabe5aaa172926e6fa69a84faa87a8
lucasgl25 <118475439+lucasgl25@users.noreply.github.com>
2023-04-24 15:13:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById('toggle-mode');
+const body = document.body;
+
+toggleBtn.addEventListener('click', function(){
+    body.classList.toggle('dark-mode')
+
+});
+
